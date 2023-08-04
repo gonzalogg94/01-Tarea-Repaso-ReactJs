5c14a6a3b1c6b2a4f960f0f51c775294045ecb1d
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-08-03 21:02:41 -0300

    agregue funcionalidad al banner de pagina principal

--- src/components/Banner/BannerCallToAction.jsx
+++ src/components/Banner/BannerCallToAction.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CallToAction from "../../data/CallToAction.json";
 
 const BannerCallToAction = () => {
+  const [banner, setBanner] = useState({});
+
+  const bannerFuncion = () => {
+    const indexAleatorio = Math.floor(Math.random() * CallToAction.length);
+    const objetoBanner = CallToAction[indexAleatorio];
+    setBanner(objetoBanner);
+  };
+
+  useEffect(() => {
+    bannerFuncion();
+    const intervalo = setInterval(() => {
+      bannerFuncion();
+    }, 4000);
+
+    // return () => clearInterval(intervalo); !!!! CONSULTAR!!!!!
+
+  }, [CallToAction.length]);
+
   return (
     <>
-      <div style={{ fontSize: "80px", fontWeight: "bold" }}>
-        HEY DEVELOPERS ..!
-      </div>
-      <div style={{ fontSize: "30px" }}>
-        Conoce los nuevos curso RollingCode!
-      </div>
+      <div style={{ fontSize: "80px", fontWeight: "bold" }}>{banner.title}</div>
+      <div style={{ fontSize: "30px" }}>{banner.subtitle}</div>
       <div style={{ marginTop: "20px" }}>
         <button className="btn btn-primary">Comprar Ahora!</button>
       </div>
